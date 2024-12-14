c49c84d18f58cea8fb4772ee4fc516fae0ad2976
Chamod Perera <chamod914@gmail.com>
2024-12-14 15:44:58 +0530

    Update

--- tests/Homepage_navigation.spec.ts
+++ tests/Homepage_navigation.spec.ts
@@ -9,55 +9,58 @@ test.beforeEach( async({ page }) => {
     expect(page).toHaveTitle(title)
 })
 
-test('Verify footer links' , async({ page }) => {
+
+test.describe('Footer test', () => {
+    test('Verify footer links' , async({ page }) => {
     
-    const footerSec = await page.locator('.main-footer')
-
-    await expect(footerSec).toBeVisible()
-
-    const footerLinks = footerSec.locator('a')
-    const linkCount = await footerLinks.count()
-
-    for(let i = 0; i < linkCount; i++) {
-        const link = footerLinks.nth(i)
-
-        await expect(link).toBeVisible();
-
-        //const linkText = await link.textContent()
-        const href = await link.getAttribute('href')
-
-        expect(href).not.toBeNull()
-        expect(href).not.toBe('')
-
-        await expect(link).toHaveAttribute('href', href)
-    }
-})
-
-
-test('Test invalid or broken link' , async({ page }) => {
-    const footerSec = page.locator('.main-footer')
-    await page.waitForSelector('.main-footer')
-    await expect(footerSec).toBeVisible()
-
-    const footerLinks = footerSec.locator('a')
-    const linkCount = await footerLinks.count()
-
-    for(let i = 0; i < linkCount; i++) {
-        const link = footerLinks.nth(i)
-
-        if(!await link.isVisible()) {
-            continue
+        const footerSec = await page.locator('.main-footer')
+    
+        await expect(footerSec).toBeVisible()
+    
+        const footerLinks = footerSec.locator('a')
+        const linkCount = await footerLinks.count()
+    
+        for(let i = 0; i < linkCount; i++) {
+            const link = footerLinks.nth(i)
+    
+            await expect(link).toBeVisible();
+    
+            //const linkText = await link.textContent()
+            const href = await link.getAttribute('href')
+    
+            expect(href).not.toBeNull()
+            expect(href).not.toBe('')
+    
+            await expect(link).toHaveAttribute('href', href)
         }
-
-        const href = await link.getAttribute('href')
-
-        const response = await page.goto(`${href}/test`)
-
-        if(response) {
-            expect(response.status()).not.toBe(200)
-        } else {
-            expect(response).not.toBeNull()
+    })
+    
+    
+    test('Test invalid or broken link' , async({ page }) => {
+        const footerSec = page.locator('.main-footer')
+        await page.waitForSelector('.main-footer')
+        await expect(footerSec).toBeVisible()
+    
+        const footerLinks = footerSec.locator('a')
+        const linkCount = await footerLinks.count()
+    
+        for(let i = 0; i < linkCount; i++) {
+            const link = footerLinks.nth(i)
+    
+            if(!await link.isVisible()) {
+                continue
+            }
+    
+            const href = await link.getAttribute('href')
+    
+            const response = await page.goto(`${href}/test`)
+    
+            if(response) {
+                expect(response.status()).not.toBe(200)
+            } else {
+                expect(response).not.toBeNull()
+            }
+            
         }
-        
-    }
+    })
 })
